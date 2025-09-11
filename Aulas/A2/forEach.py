64e7fc5596de5e5445e062587e911a83b010b180
Dario Quental <dhdq@hotmail.com>
2025-09-11 17:54:26 +0100

    melhorar comentario

--- Aulas/A2/forEach.py
+++ Aulas/A2/forEach.py
@@ -7,7 +7,8 @@ somaChave=567
 
 #mostra cada caratere em mensagem ( variavel String, Listas )
 for caraterEmMensagem in menssagem:
-       #cast de caraterEmMensagem para inteiro para somar com chave e retornar como caratere para a lista.
+       #cast de caraterEmMensagem para inteiro (ord()) para somar com chave e cast char(chr()) para retornar como caratere para a lista.
        listaCarateres.append(chr(somaChave+ord(caraterEmMensagem)))
 
+#listar
 print (listaCarateres)
